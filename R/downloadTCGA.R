70e5248be4c2bf6ce7b0e397b2e1db4ce208e1f2
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-03-06 00:25:26 +0100

    download function prototype almost finished

---
+++ R/downloadTCGA.R
@@ -0,0 +1,38 @@
+##    RTCGA package for R
+##
+#' @title download
+#'
+#' @description TODO
+#' @family RTCGA
+#' @rdname downloadTCGA
+#' @export
+downloadTCGA <- function( cancerTypes, additionalInfo = "Merge_Clinical.Level_1",
+                          writeDirectory ){
+   
+   
+   sapply( cancerTypes, function( element ){
+      gdacContent <- readLines( "http://gdac.broadinstitute.org/runs/" )
+      lastReleaseDate <- grep( pattern= "stddata__20", 
+                              x = gdacContent, 
+                              value = TRUE)[length( grep( pattern= "stddata__20", 
+                                                         x = gdacContent, 
+                                                         value = TRUE ))]
+      lastReleaseDate <- stri_extract( str = lastReleaseDate,
+                                      regex = "stddata__201[0-9]_[0-9]{2}_[0-9]{2}" )
+      
+      elementIndex <- readLines( paste0("http://gdac.broadinstitute.org/runs/", lastReleaseDate,
+                                        "/data/", element, "/", paste0(unlist(stri_extract_all(str = "stddata__2015_02_04", 
+                                                                                               regex = "[0-9]+")), collapse = "")))
+      hrefsToData <- grep( x = elementIndex, pattern = additionalInfo, value = TRUE )
+      linksToData <- stri_extract( str = hrefsToData, 
+                    regex = "gdac.broadinstitute.org[A-Za-z0-9\\.\\_]+" )[1]
+      
+      
+      
+      # this doesn't work yet
+      DATA <- getBinaryURL( linksToData, ssl.verifypeer = FALSE )
+      file.create( paste0(writeDirectory, "/", cancerTypes ) )
+      writeBin( DATA, con = paste0(writeDirectory, "/", cancerTypes ) )
+   }
+   )
+}
---
+++ R/mergeTCGA.R
@@ -0,0 +1,21 @@
+##    RTCGA package for R
+##
+#' @title Merge
+#'
+#' @description TODO
+#' @family RTCGA
+#' @rdname mergeTCGA
+#' @export
+mergaTCGA <- function( clinical, names ){
+   assertthat::assert_that( is.list( names ) )
+   assertthat::assert_that( is.character( clinical ) )
+}
+
+
+
+
+
+
+
+
+
