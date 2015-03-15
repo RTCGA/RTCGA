d2a85bfdc3dc1b6040babade00db01cd0a0bf228
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-03-15 13:19:44 +0100

    2 more functions: for checking genes availabile names. what counts is not the quality but quantity  ...

---
+++ R/availableGenesNames.R
@@ -0,0 +1,42 @@
+##    RTCGA package for R
+##
+#' @title TCGA genes' names and availability in \code{Merge_rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3} dataset
+#'
+#' @description TODO
+#' 
+#' @param rnaseqDir TODO
+#' @param genes TODO
+#' 
+#' @return TODO
+#' 
+#' @examples
+#' \dontrun{
+#' 
+#' }
+#' 
+#' @family RTCGA
+#' @rdname availableGenesNames
+#' @export
+checkGenesNamesAvailability <- function( rnaseqDir, genes ){
+    
+    assert_that( is.character( rnaseqDir ) & length( rnaseqDir ) == 1 )
+    assert_that( is.character( genes ) & length( genes ) > 0 )
+    
+    availableGenesNames( rnaseqDir, genes )
+    sapply( genes, function(element) 
+        grep( pattern = element, 
+              x = rnaseqv2[-1,1], 
+              value = TRUE )    
+    )
+}
+
+#' @export
+availableGenesNames <- function( rnaseqDir, genes ){
+    
+    assert_that( is.character( rnaseqDir ) & length( rnaseqDir ) == 1 )
+    assert_that( is.character( genes ) & length( genes ) > 0 )
+    
+    fread( rnaseqDir, select = c(1), 
+           data.table = FALSE,
+           colClasses = "character")[-1,1]
+}
