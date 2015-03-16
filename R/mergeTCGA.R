3953cebb8da2544a7817c9fe3c36a0dd2bead198
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-03-17 00:34:48 +0100

    that was a one long and hard (data.tables package isn't for normal people) function mergeTCGA_clinical_rnaseq - need to be tested #1 #2 #11

--- R/mergeTCGA.R
+++ R/mergeTCGA.R
@@ -3,10 +3,18 @@
 #' @title Merge
 #'
 #' @description TODO
+#' 
+#' @param clinicalDir A directory to a \code{cancerType.clin.merged.txt} file. 
+#' \code{cancerType} might be \code{BRCA, OV} etc.
+#' @param rnaseqDir A directory to a \code{cancerType.rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.data.txt} file
+#' 
+#' 
+#' @return A new \code{.txt} file is 
+#' 
 #' @family RTCGA
 #' @rdname mergeTCGA
 #' @export
-mergaTCGA <- function( clinical, names ){
+mergaTCGA <- function( clinicalDir, names ){
    assertthat::assert_that( is.list( names ) )
    assertthat::assert_that( is.character( clinical ) )
 }
@@ -14,8 +22,73 @@ mergaTCGA <- function( clinical, names ){
 
 
 
-
-
-
-
+#' @family RTCGA
+#' @rdname mergeTCGA
+#' @export
+mergeTCGA_clinical_rnaseq <- function( clinicalDir, rnaseqDir,
+                                       genes = c("MDM2") ){
+    
+    assert_that( is.character(clinicalDir) & length(clinicalDir) == 1)
+    assert_that( is.character(rnaseqDir) & length(rnaseqDir) == 1)
+    assert_that( is.character(genes) & length(genes) > 0)
+    
+    rnaseqv2 <- fread( rnaseqDir )
+    
+    
+    # column names are not unique :|
+    rnaseqv2 <- rnaseqv2[,unique(names(rnaseqv2)),with=FALSE]
+    
+    rnaseqv2 %>% setnames( x=.,
+                           old = names(rnaseqv2), 
+                           new =  c("HybridizationREF", 
+                                    gsub(  ".", 
+                                           "-", 
+                                           names(rnaseqv2)[-1],
+                                           fixed = TRUE)
+                                    ) %>%
+        substr(1,12) )
+    
+    realGenesNames <- genes %>% sapply( function(element) 
+        {grep( pattern = element, 
+              x = rnaseqv2[[1]], 
+              value = TRUE ) }) %>%
+        unlist( )
+    
+    
+    
+    
+    rnaseqv2_short <-  rnaseqv2 %>%
+        filter( Hybridizatio %in% realGenesNames )
+    
+    
+    clin.merged <- fread( clinicalDir, nrows = 21)[21, -1, with = FALSE] %>% 
+        toupper() %>% 
+        data.frame( barcode = . )
+    
+    
+    
+    patientsOrder <- clin.merged %>%
+        sapply( function(element){
+            grep( x = names(rnaseqv2)[-1], pattern = element, value = TRUE)[1]
+        })
+    
+    is.na(patientsOrder)
+    
+    rnaseqv2_short_frame <- cbind( data.frame( barcode = names(rnaseqv2_short) ),
+                                       as.data.frame(t(rnaseqv2_short)) ) 
+    # need to remove warning somehow
+    
+    joinedFrames <- left_join(clin.merged, rnaseqv2_short_frame[-1,], by = "barcode") 
+    # ok rows are copied now... so we need to remove some of them 
+ 
+    joinedFrames <- unique( data.table(joinedFrames), by = "barcode" )
+    
+    for( i in 1:(ncol(joinedFrames)-1)){
+    
+        write.table( file = clinicalDir, append = TRUE, x = c(as.character(rnaseqv2_short_frame[1,i+1]), 
+                                                              as.character(joinedFrames[[i+1]])) )
+        
+    }    
+    
+}
 
