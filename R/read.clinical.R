da552dfade9b71d67877f8e7bbb55fa65a254aaf
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-03-19 23:05:07 +0100

    read.clinical function #1

---
+++ R/read.clinical.R
@@ -0,0 +1,27 @@
+##    RTCGA package for R
+##
+#' @title Read from txt fo;e
+#'
+#' @description TODO
+#' 
+#' @param clinicalDir A directory to a \code{cancerType.clin.merged.txt} file. 
+#' \code{cancerType} might be \code{BRCA, OV} etc.
+#' 
+#' 
+#' @return A data.frame with clinical data.
+#' 
+#' @family RTCGA
+#' @rdname read.clinical
+#' @export
+read.clinical <- function( clinicalDir, ...){
+    
+    assert_that( is.character(clinicalDir) & length(clinicalDir) ==1 )
+    comboClinical <- read.delim( clinicalDir )
+    
+    colNames <- comboClinical[,1]
+    
+    comboClinical <- as.data.frame(t(comboClinical[, -1]), ...)
+    names(comboClinical) <- colNames
+    
+    return(comboClinical)
+}
