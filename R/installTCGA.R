8821b912977a8446d03b9a8eaa0d4fe6ce697b35
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2016-01-10 18:28:32 +0100

    bioccheck fixes

--- R/installTCGA.R
+++ R/installTCGA.R
@@ -5,7 +5,7 @@
 #' 
 #' @param packages A character specifing the names of the data packages to be installed. By default installs all packages.
 #' @param build_vignettes Should vignettes be build.
-#' @param ... Further arguments passed to \link{install_devtools}.
+#' @param ... Further arguments passed to \link[devtools]{install_github}.
 #' 
 #' @author 
 #' Marcin Kosinski, \email{m.p.kosinski@@gmail.com}
